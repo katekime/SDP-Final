9e7ce317100156933b84b770320afd20b0d9cb0c
katekime <katekime07@gmail.com>
2025-11-10 20:47:00 +0500

    adilbek's first commit

---
+++ src/main/java/org/example/Order.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.example.observer.Observer;
+import org.example.strategy.Strategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private String status;
+    private List<Observer> observers = new ArrayList<>();
+    private Strategy paymentStrategy;
+
+
+}
